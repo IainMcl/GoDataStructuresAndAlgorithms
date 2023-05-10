33c81e2ca20fbca5cf54fda283040262c52a97f5
IainMcl <iain.mclaughlan@capita.com>
2023-05-10 22:40:48 +0100

    testing queue

--- queue_test.go
+++ queue_test.go
@@ -5,7 +5,19 @@ import (
 )
 
 func TestQueue(t *testing.T){
-    list := make(Queue)
+    queue := Queue{}
+
+    queue.Enqueue(10)
+    queue.Enqueue(42)
+    queue.Enqueue(38)
+
+    if queue.length != 3{
+        t.Error("Length not equal to 3")
+    }
+
+    if queue.Peek() != 38{
+        t.Error()
+    }
 
 
 }
